c31f8673ce0d09a9ec02a171ede636b19e583eb4
kongye-su <1752604028@qq.com>
2023-05-17 18:59:29 +0800

    steal life accumulation

--- mods-unpacked/kongye59-PercentageLifeSteal/extensions/singletons/run_data.gd
+++ mods-unpacked/kongye59-PercentageLifeSteal/extensions/singletons/run_data.gd
@@ -1,11 +1,16 @@
 extends "res://singletons/run_data.gd"
 
+const min_steal_life = 1
 var cur_weapon_lifesteal:float = 0.0
+var cur_life_steal:float = 0.0
 
 func add_weapon_dmg_dealt(pos:int, dmg_dealt:int)->void :
-	if weapons.size() > pos:
-		weapons[pos].dmg_dealt_last_wave += dmg_dealt
-	emit_signal("lifesteal_effect", dmg_dealt * cur_weapon_lifesteal)
+	.add_weapon_dmg_dealt(pos,dmg_dealt)
+	
+	cur_life_steal += dmg_dealt * cur_weapon_lifesteal
+	if cur_life_steal >= min_steal_life:
+		emit_signal("lifesteal_effect", int(cur_life_steal))
+		cur_life_steal -= int(cur_life_steal)
 
 
 func manage_life_steal(weapon_stats:WeaponStats)->void :
